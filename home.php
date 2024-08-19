0753e85f1881c37450fed830822c463c3d929d67
Trần Mạnh Thắng <sieunhanga7749@gmail.com>
2024-08-19 16:19:14 +0700

    upfile part1.php

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Part1</title>
+</head>
+
+<body>
+    <?php
+    echo '<p style="text-align: center; color: green;"><b>Hello Mạnh Thắng</b></p>';
+    echo '<button><a target="_blank" href ="https://www.youtube.com/">Youtube</a></button>';
+    ?>
+
+</body>
+
+</html>
